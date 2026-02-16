79a1cd5664643bfafc82d349a6d2ca0281b835da
Yaremkevych <137343042+Yaremkevych@users.noreply.github.com>
2026-02-16 19:32:24 +0100

    Update movieService.ts

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -15,7 +15,7 @@ export default async function fetchMovies(
     const API_TOKEN: string = import.meta.env.VITE_TMDB_TOKEN;
 
     const response = await axios.get<MovieArray>(
-        `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`,
+        `https://api.themoviedb.org/3/search/movie`,
         {
             params: {
                 query: query,
